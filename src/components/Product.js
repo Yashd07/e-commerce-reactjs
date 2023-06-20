9808bf9e54793ca05e1eb488f65ad12a54bcee33
Yashd07 <yashd1272@gmail.com>
2023-06-20 16:46:14 +0530

    initial commit with Product feature in which products are fetch from API

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import {Link} from 'react-router-dom';
+import {BsPlus, bsEyeFill} from 'react-icons/bs';
 
-const Product = () => {
-  return <div>Product</div>;
+const Product = ({product}) => {
+  console.log(product);
+  const{id, image, category, title, price} =product;
+  return <div>
+    <div className='border border-[#e4e4e4] h-[300px] mb-4 relative overflow-hidden group transition'>
+      <div className='w-full h-full flex justify-center '>
+        <div className='w-[200px] mx-auto flex justify-center items-center'>
+          <img className='max-h-[160px] group-hover:scale-110 transition duration-300' src={image} alt="ele img" />
+        </div>
+      </div>
+    </div>
+    <div>2</div>
+  </div>;
 };
 
 export default Product;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import { ProductContext } from '../contexts/ProductContext';
+import { useContext } from 'react';
+import Product from '../components/Product'
 
 const Home = () => {
-  return <div>Homepage</div>;
+  //get product from context
+  const { products } = useContext(ProductContext);
+  //console.log(products);
+
+  const filProducts = products.filter((item) => {
+    return item.category === "electronics";
+  });
+
+  console.log(filProducts);
+  return (<div>
+    <section className='py-16'>
+      <div className='container mx-auto'></div>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:gridcols-5 gap-[30px] max-w-sm md:max-w-none mx-auto md:mx-4'>
+        {filProducts.map(product => {
+          return <Product product={product} key={product.id}/>
+        })}
+      </div>
+    </section>
+  </div>);
 };
 
 export default Home;
